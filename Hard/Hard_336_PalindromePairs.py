b02c9d5fd9baa06644c091dc110f273da47d90c1
KeremDurdabak <keremalp0@hotmail.com>
2022-10-21 07:28:16 -0300

    Optimized Hard question added

--- Hard/Hard_336_PalindromePairs.py
+++ Hard/Hard_336_PalindromePairs.py
@@ -2,8 +2,8 @@ class Solution:
     def palindromePairs(self, words: List[str]) -> List[List[int]]:
         pairs = []
         for i in range(len(words)):
-            for j in range(len(words)):
+            for j in range(i+1):
                 if i==j: continue
-                if ((words[i]+words[j]) == (words[i]+words[j])[::-1]):
-                    pairs.append([i,j])
+                if ((words[i]+words[j]) == (words[i]+words[j])[::-1]): pairs.append([i,j])
+                if ((words[j]+words[i]) == (words[j]+words[i])[::-1]): pairs.append([j,i])
         return pairs
